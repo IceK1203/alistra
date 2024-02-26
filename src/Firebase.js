d5e3b66e2d0d75e5bf7541d5bd8b9ed8e80c4c95
Ice <88745683+IceK1203@users.noreply.github.com>
2024-02-26 16:36:45 -0300

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -6,25 +6,14 @@ import { getStorage } from "firebase/storage";
 // Firebase Test Server
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBtLri6p_NTVzMmBQXktkbu8Z_OBzW63M8",
-    authDomain: "alistra-testserver.firebaseapp.com",
-    projectId: "alistra-testserver",
-    storageBucket: "alistra-testserver.appspot.com",
-    messagingSenderId: "1055020164835",
-    appId: "1:1055020164835:web:3394ec4306d453b65787d6"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
-// Firebase Production Server
-/*
-const firebaseConfig = {
-  apiKey: "AIzaSyDmiP7QUnrOuqojIquQBHDr8Nni-nm_HhI",
-  authDomain: "alistra-96b8d.firebaseapp.com",
-  projectId: "alistra-96b8d",
-  storageBucket: "alistra-96b8d.appspot.com",
-  messagingSenderId: "745264650134",
-  appId: "1:745264650134:web:cee77edb5e8d885444afb7"
-};
-*/
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
